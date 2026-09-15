dde1ea72c3c685509b5b442d44b7eeb403a9c2c8
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2026-09-15 20:57:54 +0530

    Create the revision list and practice the reverseString Problem

--- Normal_File/script.js
+++ Normal_File/script.js
@@ -1,1 +1,19 @@
-console.log("Hello World ");
+
+
+function ReversedString(str){
+
+    let result =""
+
+    for(let i = str.length-1; i>=0; i--){
+
+        result +=str[i]
+    }
+
+    return result ;
+
+
+}
+
+console.log(ReversedString([1,2,3]));
+ 
+
