5e5392f5e283bf9ffafecd0f6272cd0d2005c342
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2026-09-21 04:42:42 +0530

    Create the React Project Structure

---
+++ React/src/App.tsx
@@ -0,0 +1,21 @@
+import './index.css'
+
+function App() {
+
+
+
+
+  return (
+    <>
+      
+      <div className='bg-blue-300 text-white'>
+
+         <h1>Hello World!</h1>
+      </div>
+    
+      
+    </> 
+  )
+}
+
+export default App
